b26ed9fed425797b35bc4060a450181de12c97f9
slavivo <slavivo4@fit.cvut.cz>
2022-10-24 09:52:40 +0200

    Added primitive backend error handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from sanic import Sanic, response
 from sanic.config import Config
 from sanic.log import logger
-from sanic.exceptions import SanicException 
+from sanic.exceptions import ServerError 
 
 from yolo.export_yolov5 import YoloV5Exporter
 from yolo.export_yolov6 import YoloV6Exporter
@@ -70,12 +70,12 @@ async def upload_file(request):
         try:
             exporter = YoloV5Exporter(conv_path, filename, input_shape, conv_id)
         except Exception as e:
-            raise SanicException(message="Error while loading model", status_code=520)
+            raise ServerError(message="Error while loading model", status_code=520)
     elif version == "v6":
         try:
             exporter = YoloV6Exporter(conv_path, filename, input_shape, conv_id)
         except Exception as e:
-            raise SanicException(message="Error while loading model", status_code=520)
+            raise ServerError(message="Error while loading model", status_code=520)
     else:
         raise ValueError(f"Yolo version {version} is not supported.")
     
@@ -83,31 +83,31 @@ async def upload_file(request):
     try:
         exporter.export_onnx()
     except Exception as e:
-        raise SanicException(message="Error while converting to onnx", status_code=521)
+        raise ServerError(message="Error while converting to onnx", status_code=521)
 
     conversions[conv_id] = "onnx"
     try:
         exporter.export_openvino(version)
     except Exception as e:
-        raise SanicException(message="Error while converting to openvino", status_code=522)
+        raise ServerError(message="Error while converting to openvino", status_code=522)
 
     conversions[conv_id] = "openvino"
     try:
         exporter.export_blob()
     except Exception as e:
-        raise SanicException(message="Error while converting to blob", status_code=523)
+        raise ServerError(message="Error while converting to blob", status_code=523)
 
     conversions[conv_id] = "blob"
     try:
         exporter.export_json()
     except Exception as e:
-        raise SanicException(message="Error while making json", status_code=524)
+        raise ServerError(message="Error while making json", status_code=524)
 
     conversions[conv_id] = "json"
     try:
         zip_file = exporter.make_zip()
     except Exception as e:
-        raise SanicException(message="Error while making zip", status_code=525)
+        raise ServerError(message="Error while making zip", status_code=525)
 
     conversions[conv_id] = "zip"
 
