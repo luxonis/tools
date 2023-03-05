71611b19f84c533dbb187a7b97619eaf56904441
Honza Čuhel <79118988+HonzaCuhel@users.noreply.github.com>
2023-03-05 22:08:45 +0100

    Add control for field nShaves (#37)
    Co-authored-by: Jan Čuhel <jcuhel98@gmail.com>

--- yolov7/main.py
+++ yolov7/main.py
@@ -23,6 +23,7 @@ conversions = manager.dict()
 app.config.workdir = Path(__file__).parent / "tmp"
 app.config.workdir.mkdir(exist_ok=True)
 app.config.REQUEST_MAX_SIZE = 300_000_000
+DEFAULT_NSHAVES = 6
 
 
 @app.get("/yolov7/progress/<key>")
@@ -36,7 +37,7 @@ async def upload_file(request):
     logger.info(f"CONVERSION_ID: {conv_id}")
     conversions[conv_id] = "new"
 
-    nShaves = request.form["nShaves"][0]
+    nShaves = request.form["nShaves"][0] if "nShaves" in request.form else DEFAULT_NSHAVES
     logger.info(f"nShaves: {nShaves}")
 
     imgsz = request.form["inputshape"][0]
