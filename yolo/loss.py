3dd16ca365cb49df2aacde11abca3f52b69de045
HonzaCuhel <jan.cuhel@protonmail.com>
2024-07-24 12:08:32 +0200

    Add v10 loss loading

---
+++ yolo/loss.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append("./yolo/ultralytics")
+
+from ultralytics.nn.tasks import v8DetectionLoss
+import torch
+
+class v10DetectLoss:
+    def __init__(self, model):
+        self.one2many = v8DetectionLoss(model, tal_topk=10)
+        self.one2one = v8DetectionLoss(model, tal_topk=1)
+    
+    def __call__(self, preds, batch):
+        one2many = preds["one2many"]
+        loss_one2many = self.one2many(one2many, batch)
+        one2one = preds["one2one"]
+        loss_one2one = self.one2one(one2one, batch)
+        return loss_one2many[0] + loss_one2one[0], torch.cat((loss_one2many[1], loss_one2one[1]))
